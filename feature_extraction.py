4c2e8e28e97bd0275fa4c75eedac9969302acee1
vikasreddy11 <vikasreddy1090@gmail.com>
2026-03-03 15:55:55 +0530

    Added Data agumentation

--- feature_extraction.py
+++ feature_extraction.py
@@ -15,6 +15,9 @@ val_accs,val_losses=[],[]
 train_transforms=torchvision.transforms.Compose([
     torchvision.transforms.Resize((224,224)),
     torchvision.transforms.RandomHorizontalFlip(),
+    torchvision.transforms.RandomRotation(10),
+    torchvision.transforms.ColorJitter(
+        brightness=0.2,contrast=0.2,saturation=0.2),
     torchvision.transforms.RandomCrop(224,padding=4),
     torchvision.transforms.ToTensor(),
     torchvision.transforms.Normalize([0.485,0.456,0.406],
